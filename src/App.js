671cd96b8eb3210ebae72be9e65371c243fed042
Gérald Barbosa <barbosa.gerald@gmail.com>
2019-11-13 22:49:36 +0100

    add movie form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      poster: '',
+      comment: '',
+    }
+    this.onChange = this.onChange.bind(this);
+    this.submitForm = this.submitForm.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  submitForm(e) {
+    e.preventDefault();
+    const config = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(this.state),
+    };
+    const url = "https://post-a-form.herokuapp.com/api/movies/";
+    fetch(url, config)
+      .then(res => res.json())
+      .then(res => {
+        if (res.error) {
+          alert(res.error);
+        } else {
+          alert(`Film ajouté avec l'ID ${res}!`);
+        }
+      }).catch(e => {
+        console.error(e);
+        alert("Erreur lors de l'ajout d'un film");
+      });
+  }
+
+  render() {
+    return(
+      <div className="FormMovie">
+      <h1>Saisie d'un film</h1>
+
+      <form onSubmit={this.submitForm}>
+        <fieldset>
+          <legend>Informations</legend>
+          <div className="form-data">
+            <label htmlFor="title">Nom du film</label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              onChange={this.onChange}
+              value={this.state.title}
+            />
+          </div>
+
+          <div className="form-data">
+            <label htmlFor="poster">URL du poster</label>
+            <input
+              type="text"
+              id="poster"
+              name="poster"
+              onChange={this.onChange}
+              value={this.state.poster}
+            />
+          </div>
+
+          <div className="form-data">
+            <label htmlFor="comment">Commentaire</label>
+            <input
+              type="text"
+              id="comment"
+              name="comment"
+              onChange={this.onChange}
+              value={this.state.comment}
+            />
+          </div>
+          <hr />
+          <div className="form-data">
+            <input type="submit" value="Envoyer" />
+          </div>
+        </fieldset>
+      </form>
     </div>
-  );
+  )}
 }
 
 export default App;
